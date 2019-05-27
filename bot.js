d9d0bf44f370a5aba7c7c23b6361e148f392cb66
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2019-05-27 18:22:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -175,7 +175,7 @@ client.on('message', message => {
            console.log(`ON ${client.guilds.size} Servers '     Script By : Diamond Codes ' `);
          console.log(`----------------`);
        console.log(`Logged in as ${client.user.tag}!`);
-     client.user.setGame(`-help | HighLife | By X_KillerYT`)//حقوق دايموند كودز
+     client.user.setGame(`-help | AMR | By X_KillerYT`)//حقوق دايموند كودز
      client.user.setStatus("online")
      
      });
